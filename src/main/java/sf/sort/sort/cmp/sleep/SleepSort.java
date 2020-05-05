050bc06ebd7753ce99c4b83e45f92452fe4a38f7
张伟豪 <281526332@qq.com>
2020-05-05 17:01:07 +0800

    睡眠排序23333

---
+++ src/main/java/sf/sort/sort/cmp/sleep/SleepSort.java
@@ -0,0 +1,34 @@
+package sf.sort.sort.cmp.sleep;
+
+import org.junit.Test;
+// 最垃圾的排序,睡的时间最短最先输出
+public class SleepSort {
+    public static class SortThread extends Thread{
+        private int value;
+
+        public SortThread(int value) {
+            this.value = value;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(value);
+                System.out.println(value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    @Test
+    public void test1() throws InterruptedException {
+
+        int [] arr ={10,100,50,30,60};
+        for (int i : arr) {
+            new SortThread(i).start();
+        }
+        Thread.sleep(10000);
+    }
+}
